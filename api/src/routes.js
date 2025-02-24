2578d5ded75e47e200a3e62509e0dd7ca5f3b237
Henricoramalho <henrico.ramalho46@gmail.com>
2025-02-24 15:35:22 -0300

    adicionada comida

--- api/src/routes.js
+++ api/src/routes.js
@@ -2,10 +2,16 @@ const express = require('express');
 const routes = express.Router();
 
 const clientes = require('./controller/clientescontroller')
+const comida = require('./controller/comidacontroller')
 
 routes.get('/clientes', clientes.read)
 routes.post('/clientes', clientes.create)
 routes.put('/clientes/:id', clientes.update)
 routes.delete('/clientes/:id', clientes.del)
+
+routes.get('/comida', comida.read)
+routes.post('/comida', comida.create)
+routes.put('/comida/:id', comida.update)
+routes.delete('/comida/:id', comida.del)
 
 module.exports = routes
